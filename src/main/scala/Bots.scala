fc2f012f4dce331a791bc3dc283138c934c395c6
Marcus Hayward <mhayward@inviqa.com>
2018-03-08 16:24:02 +0000

    Add London bot

--- src/main/scala/Bots.scala
+++ src/main/scala/Bots.scala
@@ -1,11 +1,28 @@
 package bot
 
 import Dir._
+
 import scala.util.Random
 import Tile._
 
 trait Bot {
   def move(input: Input): Dir
+}
+
+class LondonBot extends Bot {
+  override def move(input: Input): Dir = {
+    val heroPos = input.hero.pos
+
+    val tiles: List[(Dir, Option[Tile])] = List(North, South, West, East).map(d => (d, input.game.board.at(heroPos.to(d))))
+
+    val nextMove = tiles.collectFirst {
+      case (dir, Some(Tavern)) => dir
+      case (dir, Some(Mine(id))) if id.exists(_ == input.hero.id) => dir
+      case (dir, Some(Air)) => dir
+    }
+
+    nextMove.getOrElse(Stay)
+  }
 }
 
 class RandomBot extends Bot {
