dd424c51e213b144fe0693afe9bad851e3c789bf
Marco Lopes <marco.paulo.lopes@gmail.com>
2018-04-12 15:48:03 +0100

    Commit stuff from previous weeks that those lazy arses didn't

--- src/main/scala/Bots.scala
+++ src/main/scala/Bots.scala
@@ -5,6 +5,8 @@ import Dir._
 import scala.util.Random
 import Tile._
 
+import scala.annotation.tailrec
+
 trait Bot {
   def move(input: Input): Dir
 }
@@ -30,8 +32,7 @@ class LondonBot extends Bot {
       val startNode: PathNode = PathNode(start, heuristicBetween(start, end), 0, None)
       val visited: Set[PathNode] = Set(startNode)
 
-      loop(Set(startNode), visited)
-
+      @tailrec
       def loop(open: Set[PathNode], visited: Set[PathNode]): Option[PathNode] = {
         if (open.isEmpty) None
         else {
@@ -42,17 +43,50 @@ class LondonBot extends Bot {
             val neighbours: Set[PathNode] = bestNode.pos.neighbors.collect {
               case p: Pos if board.at(p).exists(Air ==) =>
                 PathNode(p, heuristicBetween(p, end), bestNode.g + 1, Some(bestNode))
+              case p: Pos if p == end =>
+                PathNode(p, heuristicBetween(p, end), bestNode.g + 1, Some(bestNode))
             }.diff(visited)
 
-            loop(open ++ neighbours, visited + bestNode)
+            loop(open ++ neighbours - bestNode, visited + bestNode)
           }
         }
       }
+
+      loop(Set(startNode), visited)
     }
 
-    findPath(input.hero.pos, firstEnemy.pos, input.game.board)
+    // convert board to list of positioned tiles
+    val size: Int = input.game.board.size
 
-    Stay
+    def convertBoardToPositionedTiles() = ???
+
+    // 1. Move towards enemy with most mines and less health than us
+    // 2. Otherwise, move towards any enemy with less health than us
+    // 3. Otherwise, go for the closest mine
+    def findTargetPosition(): Pos = {
+      val enemies: Seq[Hero] = input.game.heroes filterNot { _.id == input.hero.id }
+      val unhealthyEnemies: Seq[Hero] = enemies.filter(_.life < input.hero.life)
+
+      if (unhealthyEnemies.isEmpty) {
+        // go to closest mine (not implement yet, so defaulting just to some hero)
+        enemies.head.pos
+      } else {
+        unhealthyEnemies.maxBy(_.mineCount).pos
+      }
+    }
+
+    @tailrec
+    def findNext(path: PathNode): Pos = path match {
+      case PathNode(p, _,_ , None) => ??? // boom crash bash!
+      case PathNode(p, _,_ , Some(PathNode(_,_ , _, None))) => p
+      case PathNode(_,_ , _, Some(x)) => findNext(x)
+    }
+
+    val path: Option[PathNode] = findPath(input.hero.pos, findTargetPosition(), input.game.board)
+    val neighbors = Set(North, South, West, East) map (x => (input.hero.pos.to(x), x))
+    val nextPos: Option[Pos] = path.map(findNext)
+
+    neighbors.find(n => nextPos.exists(n._1 ==)).map(_._2).getOrElse(Stay)
   }
 }
 class RandomBot extends Bot {
